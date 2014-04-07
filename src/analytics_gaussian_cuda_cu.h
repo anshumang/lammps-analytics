539e66b96076a5fe34d34f961eb1bc8594550dc3
Anshuman  Goswami <anshuman@titan-ext4.ccs.ornl.gov>
2014-04-07 16:20:38 -0400

    Gaussian integrated

---
+++ src/analytics_gaussian_cuda_cu.h
@@ -0,0 +1,7 @@
+#ifndef _ANALYTICS_GAUSSIAN_CUDA_CU_H_
+#define _ANALYTICS_GAUSSIAN_CUDA_CU_H_
+
+//extern "C" int gaussian_wrap_main(int argc, char **argv);
+int gaussian_wrap_main(int argc, char **argv);
+
+#endif
